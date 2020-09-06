420bd7d6fd15f21e139b5cc55409d0635af7ee71
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-09-06 22:28:06 +0100

    parsing_01

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from scholarly import scholarly
+import json
+
+app = Flask(__name__)
+
+@app.route('/search_author')
+def search_author():
+    # is the request containing JSON ?
+    print (request.is_json)
+
+    # Put json in content variable and print the name of the author
+    content = request.get_json()
+    print(content["name"])
+
+    # search for author on Google Scholar and convert results to a list from an iter
+    authors_arr = []
+    authors = scholarly.search_author(content["name"])
+    for n in authors:
+        data = {}
+        data['name'] = n.name
+        data['url_picture'] = n.url_picture
+        authors_arr.append(data)
+
+    return jsonify(authors_arr)
+    
